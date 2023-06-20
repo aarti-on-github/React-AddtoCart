ea2e9acd5a988dfbaf58ee6dfb0cd76225e94a48
Aarti Mishra <121573314+aarti-on-github@users.noreply.github.com>
2023-06-20 20:36:46 +0530

    Update ProductsApi.jsx

--- Addtocart/src/features/ProductsApi.jsx
+++ Addtocart/src/features/ProductsApi.jsx
@@ -2,7 +2,7 @@ import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
 export const ProductsApi=createApi({
     reducerPath:"ProductsApi",
-    baseQuery:fetchBaseQuery({baseUrl:"http://localhost:5000/"}),
+    baseQuery:fetchBaseQuery({baseUrl:"https://addtocartbackend.onrender.com/"}),
     endpoints:(builder)=>({
         getAllProducts:builder.query({
             query:()=> `products`,
